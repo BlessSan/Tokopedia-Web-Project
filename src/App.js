e8573a9c4dcf3a2a58c855ebf3a27d89c1ba5f03
BlessSan <87181251+BlessSan@users.noreply.github.com>
2022-01-21 03:20:13 +0800

    moved pokemon's list to app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Link, Outlet } from "react-router-dom";
 import PokemonSearch from "./components/PokemonSearch";
 import Container from "./components/container";
+import PokemonList from "./components/PokemonList";
 import "./App.css";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Link to="/pokemon-detail">Pokemon Detail</Link>
         <Link to="/my-pokemon-list">My Pokemon List</Link>
         <PokemonSearch />
+        <PokemonList />
         <Outlet />
       </Container>
     </>
