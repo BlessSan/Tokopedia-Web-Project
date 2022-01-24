f1e8b10e1a07adc49295000b62bc4a716d93299d
BlessSan <87181251+BlessSan@users.noreply.github.com>
2022-01-24 14:28:48 +0800

    added pokemonSearch as index route of pokemonDetails

--- src/components/PokemonSearch.js
+++ src/components/PokemonSearch.js
@@ -1,7 +1,39 @@
+/** @jsxImportSource @emotion/react */
+
+import { css } from "@emotion/react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const PokemonSearch = () => {
+  const container = css`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-top: 200px;
+  `;
+
+  const form = css``;
+
+  const input = css`
+    border-color: 1px black;
+    border-radius: 10px;
+    font-size: 1rem;
+    min-width: 100px;
+    padding: 0.5rem;
+    border-radius: 20px 0 0 20px;
+  `;
+
+  const button = css`
+    border-color: 1px black;
+    border-radius: 10px;
+    font-size: 1rem;
+    font-weight: 600;
+    min-width: 100px;
+    padding: 0.5rem;
+    border-radius: 0 20px 20px 0;
+    background-color: #ffffff;
+  `;
+
   const [pokemonName, setPokemonName] = useState("");
   const navigate = useNavigate();
 
@@ -15,16 +47,19 @@ const PokemonSearch = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <h1>Search for pokemon</h1>
-      <input
-        type="text"
-        value={pokemonName}
-        onChange={handleInput}
-        placeholder="e.g. bulbasaur"
-      />
-      <input type="submit" value="search" />
-    </form>
+    <div css={container}>
+      <form css={form} onSubmit={handleSubmit}>
+        <h1>Search for Pokémon</h1>
+        <input
+          css={input}
+          type="text"
+          value={pokemonName}
+          onChange={handleInput}
+          placeholder="e.g. bulbasaur"
+        />
+        <input css={button} type="submit" value="search" />
+      </form>
+    </div>
   );
 };
 
--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import MyPokemonList from "./components/MyPokemonList";
 import PokemonDetailCard from "./components/PokemonDetailCard";
 import { BrowserRouter } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
+import PokemonSearch from "./components/PokemonSearch";
 
 const ReactRouter = () => {
   return (
@@ -13,6 +14,7 @@ const ReactRouter = () => {
         <Route path="/" element={<App />}>
           <Route index element={<PokemonList />} />
           <Route path="pokemon-detail" element={<PokemonDetail />}>
+            <Route index element={<PokemonSearch />} />
             <Route path=":pokemonName" element={<PokemonDetailCard />} />
           </Route>
           <Route path="my-pokemon-list" element={<MyPokemonList />} />
